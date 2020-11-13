5918afe3bc2f48005d1ee001797b48a6fa6ed70a
Pixailz <Pixailz1@gmail.com>
2020-11-13 08:59:29 +0100

    modified:   config.xlaunch 	modified:   run 	modified:   start_gui.bat

--- start_gui.bat
+++ start_gui.bat
@@ -1,6 +1,6 @@
 @echo off
 
-wsl --distribution Ubuntu-20.04 -- ~/startgenie
+wsl --distribution Ubuntu-20.04 -- ~/.start_genie
 C:\Users\pixai\OneDrive\Bureau\config.xlaunch
 
 echo Ubuntu GUI started, you can close this Windows
